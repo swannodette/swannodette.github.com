0eb942e1f4c3db01c6e2c7f186028121433b748b
David Nolen <david.nolen@gmail.com>
2013-08-05 18:24:32 -0400

    wip

--- code/blog/src/blog/utils/dom.cljs
+++ code/blog/src/blog/utils/dom.cljs
@@ -10,5 +10,28 @@
 (defn set-class! [el class]
   (set! (.-className el) class))
 
+(defn add-class! [el name]
+  (let [cn (.-className el)]
+    (when (= (.search cn name) -1)
+      (set! (.-className el) (str cn " " name)))))
+
+(defn remove-class! [el name]
+  (let [cn (.-className el)]
+    (when-not (= (.search cn name) -1)
+      (set! (.-className el) (.replace cn name "")))))
+
+(defn tag-match [tag]
+  (fn [el]
+    (when-let [tag-name (.-tagName el)]
+      (= tag (.toLowerCase tag-name)))))
+
+(defn index-of [node-list el]
+  (loop [i 0]
+    (if (< i (count node-list))
+      (if (identical? (nth node-list i) el)
+        i
+        (recur (inc i)))
+      -1)))
+
 (defn offset [el]
   [(style/getPageOffsetLeft el) (style/getPageOffsetTop el)])
--- code/blog/src/blog/utils/reactive.cljs
+++ code/blog/src/blog/utils/reactive.cljs
@@ -2,6 +2,7 @@
   (:refer-clojure :exclude [map filter remove])
   (:require [goog.events :as events]
             [goog.events.EventType]
+            [goog.dom :as dom]
             [cljs.core.async :refer [>! <! chan put! close!]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
@@ -15,16 +16,119 @@
    :mouseout goog.events.EventType.MOUSEOUT
    :mousemove goog.events.EventType.MOUSEMOVE})
 
-(defn listen [el type]
-  (let [c (chan)]
-    (events/listen el (keyword->event-type type) #(put! c %))
-    c))
+(defn atom? [x]
+  (instance? Atom x))
+
+(defn listen
+  ([el type] (listen el type false))
+  ([el type prevent-default?]
+    (let [out (chan)]
+      (events/listen el type
+        (fn [e]
+          (if (atom? prevent-default?)
+            (when @prevent-default?
+              (.preventDefault e))
+            (when prevent-default?
+              (.preventDefault e)))
+          (put! out e)))
+      out)))
 
 (defn map [f in]
-  (let [c (chan)]
+  (let [out (chan)]
     (go (loop []
           (if-let [v (<! in)]
-            (do (>! c (f v))
+            (do (>! out (f v))
               (recur))
-            (close! c))))
-    c))
+            (close! out))))
+    out))
+
+(defn filter [pred in]
+  (let [out (chan)]
+    (go (loop []
+          (if-let [x (<! in)]
+            (do (when (pred x) (>! out x))
+              (recur))
+            (close! out))))
+    out))
+
+(defn remove [f source]
+  (let [out (chan)]
+    (go (loop []
+          (if-let [v (<! source)]
+            (do (when-not (f v) (>! out v))
+              (recur))
+            (close! out))))
+    out))
+
+(defn distinct [in]
+  (let [out (chan)]
+    (go (loop [last nil]
+          (if-let [v (<! in)]
+            (do (when (not= v last) (>! out v))
+              (recur))
+            (close! out))))
+    out))
+
+(defn fan-in [ins]
+  (let [out (chan)]
+    (go (while true
+          (let [[x] (alts! ins)]
+            (>! out x))))
+    out))
+
+(defn toggle [in]
+  (let [out (chan)
+        control (chan)]
+    (go (loop [on true]
+          (recur
+            (alt!
+              in ([v] (when on (>! out v)) on)
+              control ([v] v)))))
+    {:chan out
+     :control control}))
+
+(defn mouse-enter [el]
+  (let [matcher (el-matcher el)]
+    (->> (listen el :mouseover)
+      (filter
+        (fn [e]
+          (and (identical? el (.-target e))
+            (if-let [rel (.-relatedTarget e)] 
+              (nil? (dom/getAncestor rel matcher))
+              true))))
+      (map (constantly :enter)))))
+
+(defn mouse-leave [el]
+  (let [matcher (el-matcher el)]
+    (->> (listen el :mouseout)
+      (filter
+        (fn [e]
+          (and (identical? el (.-target e))
+            (if-let [rel (.-relatedTarget e)]
+              (nil? (dom/getAncestor rel matcher))
+              true))))
+      (map (constantly :leave)))))
+
+(defn hover [el]
+  (distinct (fan-in [(mouse-enter el) (mouse-leave el)])))
+
+(defn hover-children [el tag]
+  (let [matcher (tag-match tag)
+        matches (by-tag-name el tag)
+        over (->> (listen el :mouseover)
+               (map
+                 #(let [target (.-target %)]
+                    (if (matcher target)
+                      target
+                      (if-let [el (dom/getAncestor target matcher)]
+                        el
+                        :no-match))))
+               (remove #{:no-match})
+               (map #(index-of matches %)))
+        out (->> (listen el :mouseout)
+              (filter
+                (fn [e]
+                  (and (matcher (.-target e))
+                       (not (matcher (.-relatedTarget e))))))
+              (map (constantly :clear)))]
+    (distinct (fan-in [over out]))))
