b250da5d051536c33dda2ba32167f3a0c1e24c1c
David Nolen <david.nolen@gmail.com>
2013-08-12 21:37:21 -0400

    fix dochan

--- code/blog/src/blog/utils/macros.clj
+++ code/blog/src/blog/utils/macros.clj
@@ -1,10 +1,11 @@
 (ns blog.utils.macros)
 
 (defmacro dochan [[binding chan] & body]
-  `(cljs.core.async.macros/go
-     (loop []
-       (if-let [~binding (cljs.core.async/<! ~chan)]
-         (do
-           ~@body
-           (recur))
-         :done))))
+  `(let [chan# ~chan]
+     (cljs.core.async.macros/go
+       (loop []
+         (if-let [~binding (cljs.core.async/<! chan#)]
+           (do
+             ~@body
+             (recur))
+           :done)))))
