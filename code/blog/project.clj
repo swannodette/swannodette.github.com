f666dd824c5c3ee4972393a8221a3e78099196fa
dnolen <dnolen@cognitect.com>
2015-07-31 13:12:59 -0400

    wip

--- code/blog/project.clj
+++ code/blog/project.clj
@@ -9,7 +9,7 @@
                  "https://oss.sonatype.org/content/groups/staging/"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.27"]
+                 [org.clojure/clojurescript "1.7.28"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.omcljs/om "0.9.0"]
